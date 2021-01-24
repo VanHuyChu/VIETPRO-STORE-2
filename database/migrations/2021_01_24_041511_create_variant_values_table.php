ff2f4f3824051b384fc481dfcf1ed7cc4840b38a
VanHuyChu <vanhuyutehy@gmail.com>
2021-01-24 14:07:39 +0700

    B39+40: Migration-Seeder cho các bảng biến thể , thuộc tính

---
+++ database/migrations/2021_01_24_041511_create_variant_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVariantValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('variant_values', function (Blueprint $table) {
+            $table->integer('variant_id')->unsigned();
+            $table->foreign('variant_id')->references('id')->on('variants')->onUpdate('cascade')->onDelete('cascade');
+            
+            $table->integer('values_id')->unsigned();
+            $table->foreign('values_id')->references('id')->on('values')->onUpdate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('variant_values');
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
         $this->call(categorys::class);
         $this->call(produucts::class);
         $this->call(users::class);
+        $this->call(attribute::class);
+        $this->call(values::class);
+        $this->call(values_product::class);
+        $this->call(variant::class);
+        $this->call(variant_values::class);
     }
 }
